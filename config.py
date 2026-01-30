bb60eafdfcc5249b6ba669a7ffd153f2dfafb3a5
Alina Lozovskaya <alinailozovskaya@gmail.com>
2026-01-30 16:40:54 +0100

    test: add .gitignore and verify ignore handling

---
+++ config.py
@@ -0,0 +1,9 @@
+"""Configuration file - should be tracked"""
+
+class Config:
+    APP_NAME = "HF Sync Test"
+    VERSION = "1.0.0"
+    PORT = 7860
+    
+    # Note: Actual secrets should be in .env (which is gitignored)
+    # This file only contains non-sensitive configuration
